79f3d50ea621564308143c9fd9f6af39bc8e4dbf
daysibel1175 <estergutierres1175@gmail.com>
2025-07-26 21:00:17 -0300

    agregando styles customizados

--- Portfolio/src/theme.ts
+++ Portfolio/src/theme.ts
@@ -1,19 +1,53 @@
 import { createTheme, responsiveFontSizes } from "@mui/material";
 
+declare module "@mui/material/styles" {
+  interface Palette {
+    custom: {
+      sectionDark: string;
+      sectionLight: string;
+    };
+    customText: {
+      contrast: string;
+    };
+  }
+  interface PaletteOptions {
+    custom?: {
+      sectionDark: string;
+      sectionLight: string;
+    };
+    customText?: {
+      contrast: string;
+    };
+  }
+}
 
 let theme = createTheme({
   palette: {
-    primary: {
-      main: '#232323',
+    primary: { main: '#2e2e2e', contrastText: '#f5f5f5' },
+    secondary: { main: '#aaaaaa', contrastText: '#2e2e2e' },
+    background: {
+      default: '#ebebeb',
+      paper: '#f7f7f7',
     },
-    secondary: {
-      main: "#999999",
+    custom: {
+      sectionDark: '#2e2e2e',
+      sectionLight: '#ebebeb',
     },
+    text: {
+      primary: '#2e2e2e',
+      secondary: '#555555',
+    },
+    customText: {
+      contrast: '#f5f5f5', // texto claro sobre fundo escuro
+    },
+
   },
-  typography:{
-    fontFamily:"Poppins, sans serif"
-  }
+  typography: {
+    fontFamily: "'OpenDyslexic', 'Arial', sans-serif",
+  },
 });
+ 
+
 
 theme = responsiveFontSizes(theme)
 
